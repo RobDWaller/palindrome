31af77ea437f7ea8cc027425e79e8105af5be866
RobDWaller <rdwaller1984@googlemail.com>
2018-10-26 10:32:13 +0100

    Setup phpunit and example test

---
+++ tests/PalindromeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class PalindromTest extends TestCase
+{
+    public function testPalindrome()
+    {
+        $var1 = true;
+
+        $this->assertTrue($var1);
+    }
+}
