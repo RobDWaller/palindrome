aa2ba4307d9d195696c1cd5e1e4ccdd90c9bbfe5
RobDWaller <rdwaller1984@googlemail.com>
2018-10-26 11:31:14 +0100

    Added comments to palindrome class and method

--- src/Palindrome.php
+++ src/Palindrome.php
@@ -4,8 +4,22 @@ declare(strict_types=1);
 
 namespace App;
 
+/**
+ * A class with a single function that returns true if the string inputed is
+ * a palindrome.
+ *
+ * @author Rob Waller <rdwaller1984@gmail.com>
+ */
 class Palindrome
 {
+    /**
+     * Lowercase inputed word, compares original word against a reverse of the
+     * word. If they match then the method returns true as the word is a
+     * palindrome. 
+     *
+     * @param string $word
+     * @return bool
+     */
     public function isPalindrome(string $word): bool
     {
         return strtolower($word) === strrev(strtolower($word));
