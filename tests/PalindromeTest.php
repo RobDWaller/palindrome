a0ad72c2e99695100572c93b552216922335bfca
RobDWaller <rdwaller1984@googlemail.com>
2018-10-26 11:00:05 +0100

    Added additional tests to check method does not break

--- tests/PalindromeTest.php
+++ tests/PalindromeTest.php
@@ -21,4 +21,32 @@ class PalindromTest extends TestCase
 
         $this->assertTrue($palindrome->isPalindrome("Deleveled"));
     }
+
+    public function testIsPalindromeLowercase()
+    {
+        $palindrome = new Palindrome;
+
+        $this->assertTrue($palindrome->isPalindrome("deleveled"));
+    }
+
+    public function testIsPalindromeAlternative()
+    {
+        $palindrome = new Palindrome;
+
+        $this->assertTrue($palindrome->isPalindrome("Rotator"));
+    }
+
+    public function testIsPalindromeFalse()
+    {
+        $palindrome = new Palindrome;
+
+        $this->assertFalse($palindrome->isPalindrome("Carpark"));
+    }
+
+    public function testIsPalindromeFalseLowercase()
+    {
+        $palindrome = new Palindrome;
+
+        $this->assertFalse($palindrome->isPalindrome("football"));
+    }
 }
